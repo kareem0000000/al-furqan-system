1dec2234d219bfff8b463ba1cb67c212ff3a7e7c
kareem0000000 <118036936+kareem0000000@users.noreply.github.com>
2026-05-18 12:16:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config(); // ضروري لاستدعاء المتغيرات السحابية
+
 
 const app = express();
 
